ebfc57e6e481a728ebc1c466343313c34bb2eaad
n-hassan01 <naimul.hassan.se@gmail.com>
2024-01-15 14:49:50 +0600

    bismillah hir rahmanir rahim

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import { useRoutes } from 'react-router-dom';
+
+// routes
+import AuthenticationRoutes from './AuthenticationRoutes';
+import MainRoutes from './MainRoutes';
+
+// ==============================|| ROUTING RENDER ||============================== //
+
+export default function ThemeRoutes() {
+  // return useRoutes([MainRoutes, AuthenticationRoutes]);
+  return useRoutes([AuthenticationRoutes, MainRoutes]);
+}
